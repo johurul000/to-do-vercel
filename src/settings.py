58c4dd37f4294511e76d66e0abe4623278722025
Johurul Hassan Mazumder <johurulhassan000@gmail.com>
2024-12-13 06:44:58 +0530

    Static directory editing

--- src/settings.py
+++ src/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static', 'home')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 MEDIA_URL = '/media/' 

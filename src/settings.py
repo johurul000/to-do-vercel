21e478da13a684fff3247d8afb6258b330e7ea3f
Johurul Hassan Mazumder <johurulhassan000@gmail.com>
2024-12-13 07:05:22 +0530

    Deleted build_file.sh and editied out vercel.json

--- src/settings.py
+++ src/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static', 'home')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 MEDIA_URL = '/media/' 
